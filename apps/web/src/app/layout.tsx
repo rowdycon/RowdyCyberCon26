348f78405ccaa61b105f687941aa786bda1cfe0b
Sarthak Negi <srnegi141@gmail.com>
2025-12-19 12:31:24 -0600

    removed calling cookies in layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,21 +1,16 @@
 import "./globals.css";
 
 import { ClerkProvider } from "@clerk/nextjs";
-import { cookies } from "next/headers";
-import { defaultTheme } from "config";
 
 export default function RootLayout({
 	children,
 }: {
 	children: React.ReactNode;
 }) {
-	const theme = cookies().get("hk_theme")?.value || defaultTheme;
 	return (
 		<ClerkProvider>
 			<html lang="en">
-				<body className={theme === "dark" ? "dark" : ""}>
-					{children}
-				</body>
+				<body className={"light"}>{children}</body>
 			</html>
 		</ClerkProvider>
 	);
